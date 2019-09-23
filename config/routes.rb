2da940254e4bce56946bab17c6e7673186788e1d
shingofujita0415 <shingo-fujita0415@i.softbank.jp>
2019-09-23 18:44:34 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  Rails.application.routes.draw do
-  get 'users/new'
-
-    root 'static_pages#home'
-    get  '/help',    to: 'static_pages#help'
-    get  '/about',   to: 'static_pages#about'
-    get  '/contact', to: 'static_pages#contact'
-    get  '/signup',  to: 'users#new'
-  end
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 end
